f8cfc681e8fb26d788757a1ec229533a1c13d319
bgoncharuck <dariydragon26@gmail.com>
2026-06-12 16:11:17 +0300

    feat: splash screen into azuflutterdir

--- dart/azuflutterdir_template/lib/core/init/bootstrap.dart
+++ dart/azuflutterdir_template/lib/core/init/bootstrap.dart
@@ -2,9 +2,12 @@ import 'package:{{PACKAGE_NAME}}/core/init/init_widgets_binding.dart';
 import 'package:{{PACKAGE_NAME}}/data/service/logger/plugins/multiple_libs_logging.dart';
 import 'package:{{PACKAGE_NAME}}/core/locator.dart';
 
+import '../locator.dart';
+
 /// One-time startup (DI, services, cache). Call from [main] before [runApp] if needed.
 Future<void> bootstrap() async {
-  locator.registerSingleton<WidgetsBinding>(await initWidgetsBinding());
+  locator.registerSingleton<SplashScreenStopped>(SplashScreenStopped())
   locator.registerSingleton<LoggingLibrary>(MultipleLibrariesLogging());
   await locator.get<LoggingLibrary>().init();
+  locator.registerSingleton<WidgetsBinding>(await initWidgetsBinding());
 }
--- dart/azuflutterdir_template/lib/core/init/init_widgets_binding.dart
+++ dart/azuflutterdir_template/lib/core/init/init_widgets_binding.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<WidgetsBinding> initWidgetsBinding() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  /// turns splash screen preservation on/off
+  // widgetsBinding.preserveSplashScreen();
   return widgetsBinding;
+}
+
+class SplashScreenStopped {
+  /// file-private field + extension,
+  /// a variation of Open-Closed principle to expand functionality
+  /// without changing a class
+  var _splashScreenStopped = false;
+}
+
+extension NativeSplashPreservation on WidgetsBinding {
+  void preserveSplashScreen() {
+    FlutterNativeSplash.preserve(widgetsBinding: this);
+  }
+
+  void removeSplashScreen() {
+    /// native splash can be removed only once
+    if (locator.get<SplashScreenStopped>()._splashScreenStopped) {
+      return;
+    }
+    FlutterNativeSplash.remove();
+    locator.get<SplashScreenStopped>()._splashScreenStopped = true;
+  }
 }
